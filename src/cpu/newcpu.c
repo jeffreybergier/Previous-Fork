49ff254a126a39a37ebf9d3f71b07f9912fe84e6
andreas-g1 <andreas-g1@9ba66e4b-2233-44b8-a46a-7e93b2105c9e>
2024-10-14 16:33:49 +0000

    Syncing with latest WinUAE/Hatari.
    git-svn-id: https://svn.code.sf.net/p/previous/code/branches/branch_filesharing@1524 9ba66e4b-2233-44b8-a46a-7e93b2105c9e

--- src/cpu/newcpu.c
+++ src/cpu/newcpu.c
@@ -939,6 +939,26 @@ static void trace_cpu_disasm(void)
 	m68k_disasm_file(TraceFile, m68k_getpc (), NULL, m68k_getpc (), 1);
 }
 
+static void trace_cpu_disasm_mmu030(void)
+{
+	uaecptr new_addr = mmu030_translate(m68k_getpc(), regs.s != 0, false, false);
+
+#ifndef WINUAE_FOR_PREVIOUS
+	if (LOG_TRACE_LEVEL(TRACE_CPU_VIDEO_CYCLES)) {
+		int FrameCycles, HblCounterVideo, LineCycles;
+		Video_GetPosition ( &FrameCycles, &HblCounterVideo, &LineCycles );
+		LOG_TRACE_DIRECT_INIT ();
+		LOG_TRACE_DIRECT ( "cpu video_cyc=%6d %3d@%3d %"PRIu64" : ",
+				   FrameCycles, LineCycles, HblCounterVideo,
+				   CyclesGlobalClockCounter );
+	}
+#endif
+
+	if ( new_addr != m68k_getpc() )
+		f_out(TraceFile , "(%08x) " , m68k_getpc() );
+	m68k_disasm_file(TraceFile, new_addr, NULL, new_addr, 1);
+}
+
 void (*x_do_cycles_hatari_blitter_save)(int);
 void (*x_do_cycles_pre_hatari_blitter_save)(int);
 void (*x_do_cycles_post_hatari_blitter_save)(int, uae_u32);
