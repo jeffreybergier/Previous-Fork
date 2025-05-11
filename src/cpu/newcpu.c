85c2edf0f33e240aaec8ce4c12490435ef49ed1c
andreas-g1 <andreas-g1@9ba66e4b-2233-44b8-a46a-7e93b2105c9e>
2025-05-11 10:58:41 +0000

    Updating to latest WinUAE/Hatari.
    git-svn-id: https://svn.code.sf.net/p/previous/code/branches/branch_softfloat@1647 9ba66e4b-2233-44b8-a46a-7e93b2105c9e

--- src/cpu/newcpu.c
+++ src/cpu/newcpu.c
@@ -6045,6 +6045,12 @@ static void m68k_run_jit(void)
 	}
 #endif
 
+	if (regs.spcflags) {
+		if (do_specialties(0)) {
+			return;
+		}
+	}
+
 	for (;;) {
 #ifdef USE_STRUCTURED_EXCEPTION_HANDLING
 		__try {
