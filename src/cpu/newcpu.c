df57c351778e11539660ce4037ebf2c09dadc079
andreas-g1 <andreas-g1@9ba66e4b-2233-44b8-a46a-7e93b2105c9e>
2025-09-17 16:13:42 +0000

    Updating to latest WinUAE/Hatari.
    git-svn-id: https://svn.code.sf.net/p/previous/code/branches/branch_filesharing@1730 9ba66e4b-2233-44b8-a46a-7e93b2105c9e

--- src/cpu/newcpu.c
+++ src/cpu/newcpu.c
@@ -2487,10 +2487,15 @@ void m68k_cancel_idle(void)
 
 static void m68k_set_stop(int stoptype)
 {
-	if (regs.stopped)
+	if (regs.stopped) {
 		return;
+	}
 	regs.stopped = stoptype;
 #ifndef WINUAE_FOR_HATARI
+	if (regs.intmask == 7) {
+		gui_data.cpu_stopped = 1;
+		gui_led(LED_CPU, 0, -1);
+	}
 	if (cpu_last_stop_vpos >= 0) {
 		cpu_last_stop_vpos = vpos;
 	}
@@ -2501,6 +2506,10 @@ static void m68k_unset_stop(void)
 {
 	regs.stopped = 0;
 #ifndef WINUAE_FOR_HATARI
+	if (gui_data.cpu_stopped) {
+		gui_data.cpu_stopped = 0;
+		gui_led(LED_CPU, 0, -1);
+	}
 	if (cpu_last_stop_vpos >= 0) {
 		cpu_stopped_lines += vpos - cpu_last_stop_vpos;
 		cpu_last_stop_vpos = vpos;
@@ -3945,8 +3954,9 @@ static void cpu_halt_clear(void)
 {
 	regs.halted = 0;
 #ifndef WINUAE_FOR_HATARI
-	if (gui_data.cpu_halted) {
+	if (gui_data.cpu_halted || gui_data.cpu_stopped) {
 		gui_data.cpu_halted = 0;
+		gui_data.cpu_stopped = 0;
 		gui_led(LED_CPU, 0, -1);
 	}
 #endif
